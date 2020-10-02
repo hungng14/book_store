001138f641c078ff102078c09dc622d1bf88f15c
hungnv1 <hungnv1@runsystem.net>
2020-10-02 09:22:55 +0700

    update category

--- middlewares/category.js
+++ middlewares/category.js
@@ -5,7 +5,7 @@ const {
 } = require('./validators/category');
 
 const { responseError } = require('../utils/shared');
-const { checkBodyValidator } = require('./validators/_utils');
+const { checkBodyValidator, checkQueryValidator } = require('./validators/_utils');
 
 class ValidatorMiddleware {
     createMiddleware(req, res, next) {
@@ -22,6 +22,12 @@ class ValidatorMiddleware {
 
     checkOIdMiddleware(req, res, next) {
         const errorValidator = checkBodyValidator(req, categoryOIdValidator);
+        if (errorValidator) return res.json(responseError(1001, errorValidator));
+        return next();
+    }
+
+    checkQueOIdMiddleware(req, res, next) {
+        const errorValidator = checkQueryValidator(req, categoryOIdValidator);
         if (errorValidator) return res.json(responseError(1001, errorValidator));
         return next();
     }
