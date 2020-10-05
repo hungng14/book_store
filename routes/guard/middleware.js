c2709bd73e166761b496d9b7e6d9e8b763cb1feb
hungnguyen <hungnguyendnipr101196@gmail.com>
2020-10-05 23:03:46 +0700

    update token

--- routes/guard/middleware.js
+++ routes/guard/middleware.js
@@ -5,12 +5,16 @@ const utils = require('../../utils/utils');
 module.exports = (router, verifyProp) => {
     router.use(async (req, res, next) => {
         try {
+            console.log('ok')
             const { token } = req.user || {};
+            console.log(req.user)
+            console.log(req.session)
             const decoded = await configJWT[verifyProp](token);
             req.decoded = decoded;
             utils.setDecoded(decoded);
             return next();
         } catch (error) {
+            console.log(error)
             if (req.method === 'GET') return res.render('404', { layout: false });
             const { name } = error;
             if (name === 'JsonWebTokenError') return res.json(responseError(1002, error));
